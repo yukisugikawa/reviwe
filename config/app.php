fd1489b30c5fc7a5e93c05adea0d309d18a11f74
yukisugikawa <291789sy@gmail.com>
2020-07-07 00:02:56 +0900

    Counter Cacheインストール

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        kanazaca\CounterCache\CounterCacheServiceProvider::class,
 
         /*
          * Package Service Providers...
