c4e2dc2d7134630c3833b9e4c28c6493c57ef56f
yukisugikawa <291789sy@gmail.com>
2020-07-16 23:22:05 +0900

    post投稿機能

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -4,36 +4,65 @@
 @section('content')
     @section('maincopy', '投稿してください')
 
-    <!-- 投稿フォーム -->
-    <form action="/post" method="post">
-        {{ csrf_field() }}
+  <section class="jumbotron text-center">
+    <div class="container">
+      <h1 class="jumbotron-heading">飲食店のレビューサイト</h1>
+      <p class="lead text-muted">行きつけのお店や、お勧めのお店をみんなに紹介しましょう。</p>
 
-        <!-- value仮入れ(Userモデルとリレーションするのに必要) -->
-        <input type="hidden" name="user_id" value="1">
-        @if($errors->has('title'))
-            <div class="error_msg">{{ $errors->first('title') }}</div>
-        @endif
-        <input type="text" class="form" name="title" placeholder="タイトル" value="{{ old('title') }}">
+      <!-- successメッセージ -->
+      @if (session('success'))
+      <div class="alert alert-success">
+        {{ session('success') }}
+      </div>
+      @endif
 
-        @if($errors->has('message'))
-            <div class="error_msg">{{ $errors->first('message') }}</div>
-        @endif
-        <div>
-            <textarea class="form" name="message" placeholder="メッセージ">{{ old('message') }}</textarea>
+      <!-- エラー -->
+      @if ($errors->any())
+        <div class="alert alert-danger">
+        <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+        </ul>
         </div>
-        <input type="submit" class="create" value="投  稿">
-    </form>
+      @endif
 
-    <!-- 記事描画部分 -->
+      <!-- form -->
+      {{ Form::open(['action' => 'PostController@index', 'method' => 'post' , 'enctype' => 'multipart/form-data']) }}
+        <p>タイトル<br/>{{ Form::text('title', '', ['id' => 'title', 'size' => 50]) }}</p>
+        <p>内容<br/>{{ Form::textarea('body', '', ['id' => 'body', 'size' => '50x3']) }}</p>
+        {{ Form::file('path') }}
+        <div>
+            {{ Form::submit('投稿', ['class' => 'btn btn-success btn-lg']) }}
+        </div>
+      {{ Form::close() }}
+    </div>
+  </section>
+
+    <!-- 投稿部分 -->
     @if(count($items) > 0)
         @foreach($items as $item)
-            <div class="alert alert-primary" role="alert">
-                <a href="/post/{{ $item->id }}" class="alert-link">{{ $item->title }}</a>
-                <form action="/post/{{ $item->id }}" method="POST">
-                {{ csrf_field() }}
-                <input type="hidden" name="_method" value="DELETE">
-                <input type="submit" class="delete" value="削除">
-                </form>
+            <div class="album py-5 bg-light">
+                <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                    <div class="card mb-4 shadow-sm">
+                        <img src="{{ asset('storage/post_image/' . $item->path) }}">
+                        <div class="card-body">
+                        <p class="card-text">{{ $item->title}}</p>
+                        <p class="card-text">{{ $item->body}}</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                            <button type="button" class="btn btn-sm btn-outline-secondary">編集</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary">削除</button>
+                            </div>
+                            <small class="text-muted">{{ $item->created_at}}</small>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
             </div>
         @endforeach
     @else
