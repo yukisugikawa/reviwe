ab311b1a96c58f6834964d61f9f7f3062c908224
yukisugikawa <291789sy@gmail.com>
2020-08-07 22:09:16 +0900

    controller作成

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\CommentRequest;
+
+use App\Models\Post;
+use App\Models\Comment;
+use Auth;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CommentRequest $request)
+    {
+        //ポストが持つ全てのコメントを取得
+        //post_idを取得して、それらをリレーションしているカラムに挿入
+        $comments = new Comment;
+        $comments->comment = $request->comment;
+        $comments->user_id = $request->user()->id;//postのuser_id取得
+        $post = Post::findOrFail($request->post_id);
+        $post->comments()->save($comments);
+
+        return redirect()->route('post.show', compact('post'))->with('success', '返信しました');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
+
+use App\Models\Post;
+use Image;
+
+class PostController extends Controller
+{
+    //アクセス制限、投稿を見るだけ
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index']);
+    }
+
+    public function index()
+    {
+        //postを新しい順で取得する
+        // $items = Post::with('comment')->findOrFail($post_id);
+        $items = Post::orderBy('created_at', 'desc')->get();
+        return view('post.index', ['items' => $items]);
+        // DBから取得した値（$itemの内容）をpost.indexに渡してblade側で使用
+    }
+
+    // 描画はindexでしているので、不要
+    // public function create()
+    // {
+    //     return view('post.create');
+    // }
+
+    public function store(PostRequest $request)
+    {
+        $post = new Post;
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->user_id = $request->user()->id;//postのuser_id取得
+
+        //ファイルが存在しているか
+        if($request->hasFile('path')){
+            $file = $request->file('path');//ファイルを取得
+            $filename = time() . $file->getClientOriginalName();//ファイル名取得
+            Image::make($file)
+                ->resize(300, 300)
+                ->save(public_path( 'storage/post_image/' . $filename ));
+            $post->path = $filename;
+        }
+        $post->save();
+
+        return redirect('/post')->with('success', '投稿しました！');
+    }
+
+    public function show($post_id)
+    {
+        // DBよりURIパラメータと同じIDを持つPostの情報を取得
+        // post->comenntsでリレーションを取得して並び替え
+        $post = Post::findOrFail($post_id);
+        // $post = Post::with('comment')->findOrFail($post_id);
+        $comments = $post->comments()->orderBy('created_at', 'desc')->get();
+        return view('post.show', compact('post', 'comments'));
+    }
+
+    public function edit($post_id)
+    {
+        // $post = Post::findOrFail($post_id);
+        $post = Post::with('comment')->findOrFail($post_id);
+        return view('post.edit', ['post' => $post]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $post = new Post;
+        $post->title = $request->title;
+        $post->body = $request->body;
+
+        if ($request->file('path')->isValid()) {
+            $file = $request->file('path');
+            $filename = time() . '.' . $file->getClientOriginalExtension();
+            Image::make($file)->resize(300, 300)->save( public_path('public/post_image/' . $filename ) );
+
+            $post->file = $filename;
+        }
+        $post->save();
+    }
+
+    public function destroy($id)
+    {
+        $items = Post::find($id)->delete();
+        return redirect('/post');
+    }
+
+}
